bdf5cbb801310d6b609410f2f0e7af51bea39518
Javed <live.javedkhan@gmail.com>
2025-12-12 22:58:53 +0530

    next.config updated for meta fixes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-import type { NextConfig } from "next"; 
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  htmlLimitedBots: /.*/,
+
   images: {
     domains: [
       "res.cloudinary.com",
